acbcd5f07da2625c9398c4a919d73dae6a0a8d28
brown <mosesbinkat@gmail.com>
2024-02-03 02:06:12 +0100

    data and pagination

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(eror);
+        console.log(error);
     }
 }
 
